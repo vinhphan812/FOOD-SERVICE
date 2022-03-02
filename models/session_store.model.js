2e834aaaf698e7361f625dc533bebff3f4217d97
vinhphan812 <vinhphan812@gmail.com>
2022-03-02 14:49:12 +0700

    update session_store

--- models/session_store.model.js
+++ models/session_store.model.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const sessionStoreSchema = new Schema({
-    data: String
+	data: String,
 });
 
-const SessionStore = mongoose.model("SESSION_STORE", sessionStoreSchema, "SESSION_STORE");
+const SessionStore = mongoose.model(
+	"SESSION_STORE",
+	sessionStoreSchema,
+	"SESSION_STORE"
+);
 
 module.exports = SessionStore;
