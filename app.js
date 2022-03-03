88a269feaf6541b84c54ce53534e1f1750886f4b
vinhphan812 <vinhphan812@gmail.com>
2022-03-04 00:26:33 +0700

    config

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
+// environment config from .env
 require("dotenv").config();
+
+// import module
 const express = require("express");
-const PORT = process.env.port;
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const apiRoute = require("./api/routers/index");
+// import route
+const apiRoute = require("./api/routers/index.route");
 
+//import middleware
+const sessionMiddleware = require("./middlewares/session.middleware");
+
+// get PORT
+const PORT = process.env.port || 3000;
+
+// connect database from .env
+if (process.env.db_url)
+	mongoose.connect(process.env.db_url, {
+		connectTimeoutMS: 500,
+	});
+// init app
 const app = express();
 
+// use cookies
+app.use(cookieParser(process.env.SECRET_KEY));
+
+// use middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(sessionMiddleware);
 
+// use route
 app.use("/api", apiRoute);
 
 app.get("/", (req, res) => {
