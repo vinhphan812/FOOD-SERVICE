32f0b2b7a53c4ac87a7a8099aefa267d1f349772
vinhphan812 <vinhphan812@gmail.com>
2022-03-09 05:17:22 +0700

    get food type for public router

--- api/controllers/public.controller.js
+++ api/controllers/public.controller.js
@@ -1,10 +1,31 @@
 const Food = require("../../models/food.model");
+const FoodType = require("../../models/food_type.model");
+const { ignoreModel } = require("../../utils/ignore_model");
 
 module.exports = {
 	getFoodsInRestaurant: async (req, res, next) => {
-		// const {id} = req.params;
+		try {
+			const { id } = req.params;
 
-		const data = await Food.find({ restaurant: "Cơm Hạnh Thu" });
-		res.json({ success: true, data });
+			const data = await Food.find({ restaurant: id }).populate([
+				"type",
+				"restaurant",
+			]);
+			res.json({ success: true, data });
+		} catch ({ message }) {
+			res.json({ success: false, message });
+		}
+	},
+	getFoodsType: async (req, res, next) => {
+		try {
+			const data = await FoodType.find(
+				{ is_delete: false },
+				ignoreModel(["description", "_id"])
+			);
+
+			res.json({ success: true, data });
+		} catch ({ message }) {
+			res.json({ success: false, message });
+		}
 	},
 };
--- api/routers/public.route.js
+++ api/routers/public.route.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const idValidation = require("../validations/id.validate");
 const restaurantMiddleware = require("../middlewares/public.middleware");
-const { getFoodsInRestaurant } = require("../controllers/public.controller");
+const {
+	getFoodsInRestaurant,
+	getFoodsType,
+} = require("../controllers/public.controller");
 
 const router = express.Router();
 
@@ -13,5 +16,7 @@ router.get(
 	restaurantMiddleware,
 	getFoodsInRestaurant
 );
+
+router.get("/food-type", getFoodsType);
 
 module.exports = router;
