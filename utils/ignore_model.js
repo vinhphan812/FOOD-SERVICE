efb089bf62b0fd17252f9c5c3bc8cb152e1be80a
vinhphan812 <vinhphan812@gmail.com>
2022-04-05 01:30:22 +0700

    delete file unnecessary

--- utils/ignore_model.js
+++
@@ -1,5 +0,0 @@
-module.exports.ignoreModel = function (ignoreKeys, isDeleteIgnore = true) {
-	const ignore = { is_delete: +!isDeleteIgnore };
-	for (const key of ignoreKeys) ignore[key] = 0;
-	return ignore;
-};
--- utils/make_query.js
+++
@@ -1,4 +0,0 @@
-module.exports.makeQuery = (query = {}, is_delete = false) => {
-	query.is_delete = is_delete;
-	return query;
-};
