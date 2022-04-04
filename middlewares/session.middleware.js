a5bda41946423dbf2aa5b54202ff9ce7ccf12093
vinhphan812 <vinhphan812@gmail.com>
2022-04-05 01:26:55 +0700

    update session middleware

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -3,10 +3,17 @@ const SessionStore = require("../models/session_store.model");
 module.exports = async (req, res, next) => {
 	// if (req.query.app_secret != process.env.APP_SECRET)
 	// 	return res.json({ success: false, message: "app_secret invalid" });
+	const { sessionId } = req.signedCookies;
 
-	if (!req.signedCookies.sessionId) {
-		const data = await SessionStore.create({ data: [] });
+	if (!sessionId) {
+		const data = await SessionStore.create({ data: {} });
 		res.cookie("sessionId", data.id, { signed: true });
 	}
+
+	if (!(await SessionStore.findOne({ _id: sessionId }))) {
+		const data = await SessionStore.create({ _id: sessionId, data: {} });
+		res.cookie("sessionId", data.id, { signed: true });
+	}
+	res.locals.sessionId = sessionId;
 	next();
 };
